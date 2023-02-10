d388e6a64cf4b787e1a3effcadf9516201539939
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-02-10 18:04:31 +0400

    last

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -1,26 +1,23 @@
 import { useEffect } from "react";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 import useFetch from "../../hooks/useFetch";
 
-function Layout({ children, setSettings, setCommonData }) {
+import { setCommonData } from '@/store/commonData/commonDataSlice';
+
+function Layout({ children, setSettings }) {
+  const dispatch = useDispatch();
   const { data } = useFetch(`${process.env.NEXT_PUBLIC_DATA_URL}/api/data/settings`);
   const categories = useFetch(`${process.env.NEXT_PUBLIC_DATA_URL}/api/data/category`);
   const levels = useFetch(`${process.env.NEXT_PUBLIC_DATA_URL}/api/data/level`);
   const tags = useFetch(`${process.env.NEXT_PUBLIC_DATA_URL}/api/data/tag`);
+  console.log(setCommonData)
 
   useEffect(() => {
     if (data) setSettings(data);
-    setCommonData({ categories, levels, tags });
+    dispatch(setCommonData({ categories, levels, tags }));
   }, [data, categories, levels, tags]);
 
   return <>{children}</>;
 }
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setSettings: (payload) => dispatch({ type: "SET_SETTINGS", payload }),
-    setCommonData: (payload) => dispatch({ type: "SET_COMMON_DATA", payload }),
-  };
-};
-
-export default connect(null, mapDispatchToProps)(Layout);
+export default Layout;
