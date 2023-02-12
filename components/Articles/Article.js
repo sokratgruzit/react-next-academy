32d5b8b9ed2d5ed69f99181b3855538f3b3d95f0
David Tavadze <tavadzed@gmail.com>
2023-02-12 13:07:43 +0400

    acticle

--- components/Articles/Article.js
+++ components/Articles/Article.js
@@ -7,55 +7,53 @@ import styles from "../../styles/Articles/Article.module.scss";
 
 function Article({ data }) {
   return (
-    <Link href={`/articles/${data.slug}`} legacyBehavior>
-      <a className={styles.article}>
-        <div className={styles.cornerBorderDiv}>
-          <CornerDecor />
-          <div className={styles.cornerBorder}>
-            <div className={styles.imgBox}>
-              <img
-                src={process.env.NEXT_PUBLIC_IMG_PATH + data.image?.path}
-                alt={data.image?.alt}
-                className={styles.imgAbsolute}
-              />
-              <div className={styles.pins}>
-                {data.tag?.length
-                  ? data.tag.map((tag) => {
-                      return (
-                        <div className="white-cat-item" key={tag._id + "hash"}>
-                          {tag.title}
-                        </div>
-                      );
-                    })
-                  : ""}
-                {data.category?._id && (
-                  <div className="white-cat-item-border">
-                    {data.category?._id ? data.category.title : ""}
-                  </div>
-                )}
-              </div>
+    <Link href={`/articles/${data.slug}`} className={styles.article}>
+      <div className={styles.cornerBorderDiv}>
+        <CornerDecor />
+        <div className={styles.cornerBorder}>
+          <div className={styles.imgBox}>
+            <img
+              src={process.env.NEXT_PUBLIC_IMG_PATH + data.image?.path}
+              alt={data.image?.alt}
+              className={styles.imgAbsolute}
+            />
+            <div className={styles.pins}>
+              {data.tag?.length
+                ? data.tag.map((tag) => {
+                    return (
+                      <div className="white-cat-item" key={tag._id + "hash"}>
+                        {tag.title}
+                      </div>
+                    );
+                  })
+                : ""}
+              {data.category?._id && (
+                <div className="white-cat-item-border">
+                  {data.category?._id ? data.category.title : ""}
+                </div>
+              )}
             </div>
-            <div className={styles.bottom}>
-              <div className={styles.bottomTitle}>
-                <h3 className={styles.ttl}>{data.title}</h3>
-              </div>
-              <div className="bottomLevel">
-                {data.level._id ? (
-                  <div
-                    className={`itemDot level ${data.level.title.toLowerCase()}`}
-                  >
-                    {data.level.title}
-                  </div>
-                ) : (
-                  ""
-                )}
-                <div className="itemDot">{data.language?.title}</div>
-                <div className="itemDot">{data.duration} min</div>
-              </div>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.bottomTitle}>
+              <h3 className={styles.ttl}>{data.title}</h3>
+            </div>
+            <div className="bottomLevel">
+              {data.level._id ? (
+                <div
+                  className={`itemDot level ${data.level.title.toLowerCase()}`}
+                >
+                  {data.level.title}
+                </div>
+              ) : (
+                ""
+              )}
+              <div className="itemDot">{data.language?.title}</div>
+              <div className="itemDot">{data.duration} min</div>
             </div>
           </div>
         </div>
-      </a>
+      </div>
     </Link>
   );
 }
--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,8 +1,23 @@
 import Articles from "@/components/Articles/Articles";
 import React from "react";
 
-const index = () => {
-  return <Articles />;
+import { fetchData } from "@/utils/queries";
+
+export const getStaticProps = async () => {
+  const { data: articles } = await fetchData(
+    `${process.env.NEXT_PUBLIC_DATA_URL}/api/data/articles`
+  );
+
+  return {
+    props: {
+      title: 'Articles',
+      articles
+    },
+  };
+};
+
+const index = ({ title, articles }) => {
+  return <Articles title={title} data={articles} />;
 };
 
 export default index;
