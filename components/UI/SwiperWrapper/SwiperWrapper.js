7d875806b01bd1fc86b80d34d0d1f520e7a41397
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-02-13 16:10:49 +0400

    fixed key prop error on SwiperWrapper

--- components/UI/SwiperWrapper/SwiperWrapper.js
+++ components/UI/SwiperWrapper/SwiperWrapper.js
@@ -3,41 +3,37 @@ import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
-import styles from "../../../styles/Articles/Articles.module.scss";
-
 const SwiperWrapper = ({ children, ...props }) => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    return (
-        <Swiper
-            loop={false}    
-            index={index}
-            onIndexChanged={setIndex}
-            {...props}
-            spaceBetween={30}
-            slidesPerView={3}
-            breakpoints={{
-              0: {
-                slidesPerView: 1.3,
-                spaceBetween: 16,
-              },
-              768: {
-                slidesPerView: 2.5,
-                spaceBetween: 30,
-              },
-              1023: {
-                slidesPerView: 3,
-                spaceBetween: 30,
-              },
-            }}
-        >
-            {children.map((child) => (
-                <div className={styles.item}>
-                    <SwiperSlide>{child}</SwiperSlide>
-                </div>
-            ))}
-        </Swiper>
-    );
+  return (
+    <Swiper
+      loop={false}    
+      index={index}
+      onIndexChanged={setIndex}
+      {...props}
+      spaceBetween={30}
+      slidesPerView={3}
+      breakpoints={{
+        0: {
+          slidesPerView: 1.3,
+          spaceBetween: 16,
+        },
+        768: {
+          slidesPerView: 2.5,
+          spaceBetween: 30,
+        },
+        1023: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+    >
+      {children.map((child, index) => (
+        <SwiperSlide key={index}>{child}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default SwiperWrapper;
