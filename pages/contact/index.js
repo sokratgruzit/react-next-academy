439d620649df3c12e658e345db2aea28d0e3cf3a
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-02-13 18:23:36 +0400

    added Contact page and imported button & input new versions

---
+++ pages/contact/index.js
@@ -0,0 +1,48 @@
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button.js";
+
+import styles from "../../styles/Contact/Contact.module.scss";
+
+const FORM_DATA = [
+  {
+    type: "text",
+    placeholder: "Your name",
+    label: "Name",
+  },
+  {
+    type: "text",
+    placeholder: "Type e-mail address",
+    label: "Email address",
+  },
+  {
+    type: "text",
+    placeholder: "Enter your subject",
+    label: "Subject",
+  },
+];
+
+const FROM_TEXTAREA = {
+  type: "message",
+  placeholder: "type here",
+  label: "message",
+};
+
+const Contact = () => {
+
+  return (
+    <div className={`${'contact container textStyles'} ${styles.contact}`}>
+      <img src="img/Bg/bg_42.png" alt="bg" />
+      <div className={styles.title}>
+        <h2>contact us</h2>
+        <h5>We’re here with you every step way!</h5>
+      </div>
+      <div className={styles.contactBox}>
+        <Input type="input" data={FORM_DATA} />
+        <Input type="textarea" data={FROM_TEXTAREA} />
+        <Button size={"btn-big"} color={"blue"} title="send message" className={""} />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
