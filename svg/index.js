7fa1cdf5d973b9c11104ec88f1fb5d9ac72a0974
gogaokradze <deathofsuper@gmail.com>
2023-02-14 12:27:00 +0400

    feat: added social icons

--- svg/index.js
+++ svg/index.js
@@ -1,1 +1,7 @@
 export { default as Reset } from "./Reset";
+export { default as Arrow } from "./Arrow";
+export { default as FilterBy } from "./FilterBy";
+export { default as Facebook } from "./SocialNetwork/Facebook";
+export { default as GitHub } from "./SocialNetwork/GitHub";
+export { default as Linkedin } from "./SocialNetwork/Linkedin";
+export { default as Twitter } from "./SocialNetwork/Twitter";
