b023a32a05f8603d79f156232bdb63ca6346a878
Zura Chubinashvili <chubinashvili2003@gmail.com>
2023-02-16 15:48:17 +0400

    add instructors page

---
+++ pages/instructors/index.js
@@ -0,0 +1,13 @@
+import Instructors from '../../components/Instructors/Instructors';
+import BecomeInstructorInfo from '../../components/Instructors/BecomeInstructorInfo';
+
+const index = () => {
+  return (
+    <>
+      <Instructors />
+      <BecomeInstructorInfo />
+    </>
+  );
+};
+
+export default index;
