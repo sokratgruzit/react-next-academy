5f1f188f7dcfc44c5e35b64af1a84907008ab412
NTsloth <sloth2003nt@gmail.com>
2023-02-17 12:23:56 +0400

    articles

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -5,6 +5,8 @@ import Articles from "@/components/Articles/Articles";
 import { fetchData } from "@/utils/queries";
 import ArticlesHeader from "@/components/Articles/ArticlesHeader";
 
+import styles from "../../styles/Articles/ArticleIndex.module.scss";
+
 export async function getServerSideProps(context) {
   const { data: articles } = await fetchData(
     `${process.env.NEXT_PUBLIC_DATA_URL}/api/data/articles${
@@ -23,6 +25,12 @@ export async function getServerSideProps(context) {
 const index = ({ title, articles }) => {
   return (
     <div>
+      <div className={`lightImg ${styles.imgBox}`}>
+        <img src="/img/Article/articlesBgL.png" alt="" />
+      </div>
+      <div className={`darkImg ${styles.imgBox}`}>
+        <img src="/img/Article/articlesBg.png" alt="" />
+      </div>
       <ArticlesHeader />
       <Articles title={"Articles"} data={articles} />
     </div>
