edda31bf53aca4d55d19368d51d472a62e8e124f
nikak01 <eqiza99@gmail.com>
2023-02-22 15:52:53 +0400

    update instructors page

--- pages/instructors/[id].js
+++ pages/instructors/[id].js
@@ -1,11 +1,24 @@
 import InstructorItem from "../../components/Instructors/InstructorItem";
 
-const DUMMY_DATA = {
+const DUMMY_DATA = [{
   id: 0,
-  text: "Making sure that our products exceed customer expectations for quality and performance.",
   name: "Jack Jones",
+  text: "Making sure that our products exceed customer expectations for quality and performance.",
   img: "/img/InstructorImgs/JackJones.png",
-};
+  rating: '4',
+  reviews: '9',
+  students: '25',
+  coursesSum: '4',
+  courses: [
+    {
+      title: 'Kak Kushat Xui',
+      price: 'Free',
+      level: 'Legendary',
+      language: 'ENG',
+      time: '120hrs'
+    }
+  ]
+}];
 
 const Instructor = () => {
   return <InstructorItem data={DUMMY_DATA} />;
