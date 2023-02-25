9db033dacebaac40f3d92c0f9872372c6ab47b63
keksik95 <geliashvilidato95@gmail.com>
2023-02-25 12:21:45 +0400

    update

--- components/Articles/Articles.js
+++ components/Articles/Articles.js
@@ -7,17 +7,17 @@ import styles from "../../styles/Articles/Articles.module.scss";
 
 function Articles({ title, data }) {
   return (
-      <div className={styles.list}>
-        {data?.docs?.length ? (
-          <SwiperWrapper>
-            {data?.docs.map((item, index) => (
-              <div key={index} className={styles.item}>
-                <Card data={item} type={"default"} />
-              </div>
-            ))}
-          </SwiperWrapper>
-        ) : null}
-      </div>
+    <div className={styles.list}>
+      {data?.docs?.length ? (
+        <SwiperWrapper>
+          {data?.docs.map((item, index) => (
+            <div key={index} className={styles.item}>
+              <Card data={item} type={"default"} />
+            </div>
+          ))}
+        </SwiperWrapper>
+      ) : null}
+    </div>
   );
 }
 
