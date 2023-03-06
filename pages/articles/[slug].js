5c47d8dcbcc5508506eb42e798c23e4b5b391915
nikak01 <eqiza99@gmail.com>
2023-03-06 13:16:20 +0400

    update

--- pages/articles/[slug].js
+++ pages/articles/[slug].js
@@ -35,7 +35,6 @@ export const getStaticProps = async (context) => {
     `${process.env.NEXT_PUBLIC_DATA_URL}/api/data/articles?category=62fb6bd0ab723fa8b038fcdf&limit=3`
   );
 
-  console.log(`${process.env.NEXT_PUBLIC_DATA_URL}/api/data/articles/${slug}`)
   return {
     props: {
       article,
