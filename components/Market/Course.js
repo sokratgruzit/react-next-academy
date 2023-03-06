e65f8eddee78ea180c0c590d6e8f38a4a0d4d3f8
keksik95 <geliashvilidato95@gmail.com>
2023-03-06 18:25:33 +0400

    course/Banner & Video

--- components/Market/Course.js
+++ components/Market/Course.js
@@ -1,9 +1,20 @@
-import styles from "../../styles/Market/Course.module.scss";
+import Banner from "../UI/Banner/Banner";
+import Button from "../UI/Button/Button";
+import Video from "./Video/Video";
+
+// import styles from "../../styles/Market/Course.module.scss";
 
 function Course() {
   return (
     <div className="container">
-      <div className={styles.inner}>12345</div>
+      <Banner
+        type={"course-banner"}
+        title={"introducing to programming with python and java"}
+        teacher={"davit tavadze"}
+        profileImg={"teacher"}
+        img={"banner1"}
+      />
+      <Video type={"video"} img={"courseBg"} />
     </div>
   );
 }
