9455f2ce5ed9cfed7a97205e3bb145e8152aea1e
keksik95 <geliashvilidato95@gmail.com>
2023-03-06 12:24:01 +0400

    update

---
+++ components/Market/Course.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Market/Course.module.scss";
+
+function Course() {
+  return (
+    <div className="container">
+      <div className={styles.inner}>12345</div>
+    </div>
+  );
+}
+
+export default Course;
