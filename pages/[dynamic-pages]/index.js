f0aa44b05ca45fca8c78d9f71bb1536250807f56
NTsloth <sloth2003nt@gmail.com>
2023-03-29 16:27:23 +0400

    update

--- pages/[dynamic-pages]/index.js
+++ pages/[dynamic-pages]/index.js
@@ -2,6 +2,8 @@ import parse from "html-react-parser";
 import Moment from "react-moment";
 import { useRouter } from "next/router";
 
+import NotFound from "../404";
+
 import { fetchData } from "@/utils/queries";
 
 export async function getServerSideProps(context) {
@@ -22,8 +24,8 @@ import Introduction from "@/components/Introduction/Introduction";
 
 const index = ({ content }) => {
   console.log(content);
-  return; // <NotFound />
-  // <Introduction />
+  return <NotFound />;
+  // <Introduction />;
 };
 
 export default index;
