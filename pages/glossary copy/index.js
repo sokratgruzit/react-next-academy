5ded99a2c98e6dc72ecd388cb24a0a2c82396d9b
NickNameGIoO <Tskhvaragiorgi@gmail.com>
2023-04-06 12:38:04 +0400

    add branch

---
+++ pages/glossary copy/index.js
@@ -0,0 +1,19 @@
+import { fetchData } from "@/utils/queries";
+
+import Glossaries from "@/components/Glossary/Glossaries";
+
+export const getStaticProps = async () => {
+  const { data: glossaries } = await fetchData(
+    `${process.env.NEXT_PUBLIC_DATA_URL}/api/data/glossaries`
+  );
+
+  return {
+    props: { glossaries },
+  };
+};
+
+function index({ glossaries }) {
+  return <Glossaries res={glossaries} />;
+}
+
+export default index;
